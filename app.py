41d5e2016b983d3a8575dae4c84d4d811c5f7b2c
Ekabn <endr.ermuun@email.com>
2026-07-23 11:04:42 +0800

    disable reloader fix

--- app.py
+++ app.py
@@ -247,4 +247,4 @@ def sessions():
     return jsonify(sessions_list)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
